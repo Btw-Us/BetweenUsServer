a4428a948cf5d62a963a656ce4246d585b4e7452
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-07-27 01:02:12 +0530

    refactor application entry point to consolidate module configuration

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -10,6 +10,23 @@
 
 package com.aatech
 
+import com.aatech.plugin.configureAuthentication
+import com.aatech.plugin.configureMongoDB
+import com.aatech.plugin.configureMonitoring
+import com.aatech.plugin.configureMySqlDatabases
+import com.aatech.plugin.configureRouting
+import com.aatech.plugin.configureSerialization
+import io.ktor.server.application.Application
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.mainModule() {
+    configureAuthentication()
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+    configureMySqlDatabases()
+    configureMongoDB()
 }
